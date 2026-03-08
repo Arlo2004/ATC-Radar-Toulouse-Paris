efebc0476b0d6776434b0cef1116fa43829276f4
Arlo2004 <manuelgjuan04@gmail.com>
2026-03-08 17:17:58 +0100

    Update vols_candidats.py
    À ce stade, je demande la permission à OpenSkyData de faire une requête et de lire la base de données. Le script me transporte sur une page où je dois entrer mon mot de passe pour autoriser la requête.

--- vols_candidats.py
+++ vols_candidats.py
@@ -1,12 +1,7 @@
---SQL 1: IDENTIFICATION DES CANDIDATS
-SELECT 
-    icao24 AS id_avion, 
-    callsign AS matricula,
-    firstseen AS hora_despegue
+java -jar trino_jar.jar --server https://trino.opensky-network.org --external-authentication --user arlo2004 --catalog minio --schema osky --execute "
+SELECT icao24, firstseen, lastseen 
 FROM flights_data4 
 WHERE estdepartureairport = 'LFBO' 
-  AND (estarrivalairport = 'LFPG' OR estarrivalairport = 'LFPO')
-  AND day BETWEEN 1704067200 AND 1704153600 
-  --Filtro de jours
-ORDER BY firstseen ASC
-LIMIT 10;
+AND (estarrivalairport = 'LFPG' OR estarrivalairport = 'LFPO') 
+AND day BETWEEN 1704067200 AND 1704153600 
+LIMIT 10" --output-format CSV > lista_candidatos.csv
